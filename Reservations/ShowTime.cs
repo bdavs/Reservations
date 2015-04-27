b773153c7db47b4d6b542b30e9dc02e2a8a3aca7
bdavs <rdavis2@umassd.edu>
2015-04-27 14:59:17 -0400

    Actually added ShowTime.cs

---
+++ Reservations/ShowTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations
+{
+    public class ShowTime
+    {
+        private DateTime info;
+        private string eventName;
+        private string venueName;
+
+        public DateTime Info
+        { 
+            get {return info;}
+            set {info = value;}
+        }
+        public string EventName
+        {
+            get {return eventName;}
+            set {eventName = value;}
+        }
+        public string VenueName
+        {
+            get {return venueName;}
+            set {venueName = value;}
+        }
+
+
+        public ShowTime() { }
+    }
+}
