5016cc5fb3a465053a768c4be7fc726d2187cdb4
DanGarcia595 <@umassd.edu>
2015-04-27 15:20:55 -0400

    Some stuff

--- Reservations/AdminForm.Designer.cs
+++ Reservations/AdminForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 322);
+            this.Name = "AdminForm";
             this.Text = "AdminForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
