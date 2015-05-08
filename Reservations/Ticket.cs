7a97a485348de00428fcf20f5f5957b7dd9e9635
x1drewx <drew.mele@gmail.com>
2015-05-08 12:34:23 -0400

    Tickets

---
+++ Reservations/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Reservations
+{
+    class Ticket
+    {
+        private Shows show;
+        private int seatnumber;
+
+
+        public Shows Show
+        {
+            get { return show; }
+            set { show = value; }
+        }
+        public int SeatNumber
+        {
+            get { return seatnumber; }
+            set { seatnumber = value; }
+        }
+
+        public Ticket()
+        { }
+        public Ticket(Shows s,int n)
+        {
+            show = s;
+            seatnumber = n;
+        }
+    }
+}
